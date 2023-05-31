cd270717547ceea9cef4094e29f2c13164c25c9e
Massimo Colella <massimo-colella@outlook.it>
2023-05-31 18:17:01 +0200

    es completato

--- index.php
+++ index.php
@@ -21,7 +21,7 @@ class Movie{
 
         $starPercentageRounded = round($starPercentage / 10) * 10 .'%';
 
-        $stringa = "width='$starPercentageRounded'";
+        $stringa = "width:$starPercentageRounded";
 
         $this->voto = $stringa;
     }
@@ -48,6 +48,8 @@ $array_mobies = [
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Movies</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
     <div class="container row m-auto">
@@ -64,9 +66,6 @@ $array_mobies = [
                 <li class="list-group-item">Durata: <?php echo $elem->durata ?></li>
                 <li class="list-group-item">Genere: <?php echo $elem->genere ?></li>
               </ul>
-              <div class="stars-outer">
-                <div class="stars-inner" ></div>
-            </div>
             </div>
         </div>
         <?php endforeach ?>
